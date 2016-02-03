4477e65569ff991bcd02eaa75275313d9c9fd2d0
Jani Arvonen <jani.arvonen@gmail.com>
2016-02-03 22:51:27 +0200

    model.place: add in-memory vector for places (for now nummela and helsinki)

--- src/busa_server/model/place.clj
+++ src/busa_server/model/place.clj
@@ -7,3 +7,7 @@
 
 (defn new-place [id name]
   (s/validate Place (map->Place {:id id :name name})))
+
+(def nummela (new-place "p1447" "Nummela"))
+(def helsinki (new-place "p1001" "Helsinki"))
+(def places [nummela helsinki])
