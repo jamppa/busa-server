4ee0b367b4a4ae1168838ce11b8d738b31dd42bb
Jani Arvonen <jani.arvonen@gmail.com>
2016-02-19 14:39:30 +0200

    feat(main): setup db

--- src/busa_server/main.clj
+++ src/busa_server/main.clj
@@ -1,9 +1,13 @@
 (ns busa-server.main
-  (:require [busa-server.page.driver :as page-driver])
+  (:require
+    [busa-server.page.driver :as page-driver]
+    [busa-server.model.db :as db])
   (:gen-class))
 
 (defn -main
   [& args]
   (println "Starting BUSA Server")
   (page-driver/init)
-  (print "Webdrvier initialized"))
+  (println "Webdriver initialized")
+  (db/setup-db)
+  (println "Database initialized"))
