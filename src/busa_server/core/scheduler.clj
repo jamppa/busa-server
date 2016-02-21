7ca2d6063c70b767d6706a783452577725c956c0
Jani Arvonen <jani.arvonen@gmail.com>
2016-02-21 21:50:19 +0200

    introduce core.scheduler

---
+++ src/busa_server/core/scheduler.clj
@@ -0,0 +1,16 @@
+(ns busa-server.core.scheduler
+  (:require
+    [clj-time.core :as t]
+    [clj-time.local :as l]
+    [clj-time.periodic :as p]
+    [chime :refer [chime-at]])
+  (:import [org.joda.time DateTimeZone]))
+
+(defn- after-midnight-seq []
+  (rest
+    (p/periodic-seq
+      (.. (l/local-now) (withTime 0 5 0 0)) (t/days 1))))
+
+(defn run-everyday-after-midnight [func]
+  (chime-at (after-midnight-seq)
+  (fn [time] (func))))
