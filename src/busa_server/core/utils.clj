39bbaa73302d6b8d36f1f70394b45ba1ec37931b
Jani Arvonen <jani.arvonen@gmail.com>
2016-02-21 17:22:20 +0200

    feat(core.utils): should map datetime string to millis

--- src/busa_server/core/utils.clj
+++ src/busa_server/core/utils.clj
@@ -4,9 +4,13 @@
     [clj-time.local :as l]))
 
 (def date-format (f/formatter "yyyy-MM-dd"))
+(def datetime-format (f/formatter "yyyy-MM-dd-HH:mm"))
 
 (defn date-to-iso [local-date]
   (f/unparse date-format local-date))
 
 (defn today-as-iso []
   (-> (l/local-now) date-to-iso))
+
+(defn datetime-str-to-millis [datetime-str]
+  (.getMillis (l/to-local-date-time (f/parse datetime-format datetime-str))))
--- test/busa_server/core/utils_test.clj
+++ test/busa_server/core/utils_test.clj
@@ -7,3 +7,6 @@
 
 (fact "should return today as iso string"
   (utils/today-as-iso) => (f/unparse (f/formatter "yyyy-MM-dd") (l/local-now)))
+
+(fact "should map datetime string to millis"
+  (utils/datetime-str-to-millis "2016-02-21-16:30") => 1456065000000)
