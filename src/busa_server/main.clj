41a4ec11c95be2cf713dc30938acf6f83391aa97
Jani Arvonen <jani.arvonen@gmail.com>
2016-03-31 19:38:52 +0300

    feat(server): introduce start-server

--- src/busa_server/main.clj
+++ src/busa_server/main.clj
@@ -3,7 +3,9 @@
     [busa-server.page.driver :as page-driver]
     [busa-server.model.db :as db]
     [busa-server.core.connections :as connections]
-    [busa-server.core.scheduler :as scheduler])
+    [busa-server.core.scheduler :as scheduler]
+    [busa-server.api.handler :as busa-api]
+    [org.httpkit.server :as httpkit])
   (:gen-class))
 
 (defn init []
@@ -15,7 +17,12 @@
   (println "Connections loaded")
   (scheduler/run-everyday-after-midnight connections/load-connections))
 
+(defn start-server [conf]
+  (httpkit/run-server #'busa-api/busa-handler conf)
+  (println (str "Server started at port " (:port conf))))
+
 (defn -main
   [& args]
   (println "Starting BUSA Server")
-  (init))
+  (init)
+  (start-server {:port 3000}))
