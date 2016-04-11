67241d38824711e1e61eec59a183a468b40dbc03
Jani Arvonen <jani.arvonen@gmail.com>
2016-04-11 11:46:20 +0300

    feat(client.matkahuolto): should make new connections params from places and departure date

--- src/busa_server/client/matkahuolto.clj
+++ src/busa_server/client/matkahuolto.clj
@@ -7,16 +7,31 @@
 (def connections-api-template
   "https://liput.matkahuolto.fi/minfo/mlippu_rest/connections?allSchedules=0&arrivalStopAreaId=:arrivalId&departureDate=:departureDate&departureStopAreaId=:departureId&ticketTravelType=0")
 
+(def client-options {
+  :headers {"Accept" "application/json"
+            "Accept-Language" "fi"
+            "Theme" "matkahuolto"}})
+
 (s/defrecord ConnectionsParams
-  [arrivalId    :- s/Str
-   departureId  :- s/Str
-   departureDate :- s/Str])
+  [arrival-id    :- s/Str
+   departure-id  :- s/Str
+   departure-date :- s/Str])
 
 (defn make-connections-params [keyvals]
   (s/validate ConnectionsParams (map->ConnectionsParams keyvals)))
 
+(defn to-connections-params [arrival-place departure-place departure-date]
+  (make-connections-params {
+    :arrival-id (:id arrival-place)
+    :departure-id (:id departure-place)
+    :departure-date departure-date
+    }))
+
 (defn connections-api-url [params]
   (-> connections-api-template
-    (.replace ":arrivalId" (:arrivalId params))
-    (.replace ":departureId" (:departureId params))
-    (.replace ":departureDate" (:departureDate params))))
+    (.replace ":arrivalId" (:arrival-id params))
+    (.replace ":departureId" (:departure-id params))
+    (.replace ":departureDate" (:departure-date params))))
+
+(defn fetch-todays-connections [departure-place arrival-place]
+  nil)
--- test/busa_server/client/matkahuolto_test.clj
+++ test/busa_server/client/matkahuolto_test.clj
@@ -1,16 +1,23 @@
 (ns busa-server.client.matkahuolto-test
   (:require
     [midje.sweet :refer :all]
-    [busa-server.client.matkahuolto :as m]))
+    [busa-server.client.matkahuolto :as m]
+    [busa-server.model.place :as p]))
 
-(def connections-params (busa_server.client.matkahuolto.ConnectionsParams. "p1001" "p1447" "2016-4-10"))
+(def connections-params (busa_server.client.matkahuolto.ConnectionsParams. "p1001" "p1447" "2016-04-10"))
 
 (fact "should make new connections params"
   (m/make-connections-params
-    {:arrivalId "p1001"
-     :departureId "p1447"
-     :departureDate "2016-4-10"}) => connections-params)
+    {:arrival-id "p1001"
+     :departure-id "p1447"
+     :departure-date "2016-04-10"}) => connections-params)
 
 (fact "should format connections api url from params"
   (m/connections-api-url connections-params) =>
-    "https://liput.matkahuolto.fi/minfo/mlippu_rest/connections?allSchedules=0&arrivalStopAreaId=p1001&departureDate=2016-4-10&departureStopAreaId=p1447&ticketTravelType=0")
+    "https://liput.matkahuolto.fi/minfo/mlippu_rest/connections?allSchedules=0&arrivalStopAreaId=p1001&departureDate=2016-04-10&departureStopAreaId=p1447&ticketTravelType=0")
+
+(fact "should define client options"
+  m/client-options => {:headers {"Accept-Language" "fi" "Accept" "application/json" "Theme" "matkahuolto"}})
+
+(fact "should make new connections params from places and date"
+  (m/to-connections-params p/helsinki p/nummela "2016-04-10") => connections-params)
