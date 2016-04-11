3eb0f7f99549b4a2cf1a94416d64f526d5c133a3
Jani Arvonen <jani.arvonen@gmail.com>
2016-04-11 14:18:06 +0300

    chore(client.utils): remove, not needed

--- src/busa_server/client/util.clj
+++
@@ -1,7 +0,0 @@
-(ns busa-server.client.util
-  (:require
-    [clojure.data.json :as json]
-    [clojure.walk :refer [keywordize-keys]]))
-
-(defn body [{body :body}]
-  (-> (json/read-str body) keywordize-keys))
