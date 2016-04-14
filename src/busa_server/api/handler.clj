d9e1a7399ffc295e644694d9d02c7a05b97244cd
Jani Arvonen <jani.arvonen@gmail.com>
2016-04-14 13:53:05 +0300

    chore(handler): wrap restful middleware

--- src/busa_server/api/handler.clj
+++ src/busa_server/api/handler.clj
@@ -2,10 +2,13 @@
   (:require
     [compojure.core :refer :all]
     [compojure.handler :as handler]
+    [ring.middleware.format :refer [wrap-restful-format]]
     [busa-server.api.connection-api :refer :all]))
 
 (defroutes busa-routes
   (context "/api" [] connection-api))
 
 (def busa-handler
-  (-> (handler/site busa-routes)))
+  (->
+    (handler/site busa-routes)
+    (wrap-restful-format :formats [:json-kw])))
