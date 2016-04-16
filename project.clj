1c28b9e3de739a903303b50a9de00b283fa23bea
Jani Arvonen <jani.arvonen@gmail.com>
2016-04-16 19:25:59 +0300

    chore(project): clean dependencies

--- project.clj
+++ project.clj
@@ -5,25 +5,21 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [
     [org.clojure/clojure "1.7.0"]
-    [clj-webdriver "0.7.2"]
-    [com.codeborne/phantomjsdriver "1.2.1" :exclusion [org.seleniumhq.selenium/selenium-java
-                                                          org.seleniumhq.selenium/selenium-server
-                                                          org.seleniumhq.selenium/selenium-remote-driver]]
-    [enlive "1.1.6"]
+    [prismatic/schema "1.1.0"]
     [jarohen/chime "0.1.9"]
     [clj-time "0.11.0"]
-    [prismatic/schema "1.0.4"]
     [com.apa512/rethinkdb "0.11.0"]
     [ring/ring-core "1.4.0"]
     [ring-cors "0.1.7"]
     [ring-middleware-format "0.7.0"]
     [compojure "1.4.0"]
-    [http-kit "2.1.19"]]
+    [http-kit "2.1.19"]
+    [javax.servlet/servlet-api "2.5"]]
   :main ^:skip-aot busa-server.main
   :target-path "target/%s"
   :profiles {
     :uberjar {:aot :all}
     :dev {
-      :dependencies [[midje "1.8.3"] [javax.servlet/servlet-api "2.5"] [ring/ring-mock "0.3.0"]]
+      :dependencies [[midje "1.8.3"] [ring/ring-mock "0.3.0"]]
       :plugins [[lein-midje "3.2"]]}
   })
