e8b993a5e0717484c6b46cc07a7032fd362970df
Jani Arvonen <jani.arvonen@gmail.com>
2016-08-21 20:06:24 +0300

    feat(api.connection-api): should return http status 200 when successfully finds connections departuring next

--- src/busa_server/api/connection_api.clj
+++ src/busa_server/api/connection_api.clj
@@ -3,12 +3,19 @@
     [compojure.core :refer :all]
     [ring.util.response :refer :all]
     [busa-server.api.api-utils :refer :all]
-    [busa-server.core.connections :as connections]))
+    [busa-server.core.connections :as connections]
+    [busa-server.model.place :as place]))
 
 (def prefix "/connections")
 
 (def departuring-next
   (GET (str prefix "/departuring_next") [:as req]
-  (-> (connections/find-all-connections-departuring-next) response)))
+    (-> (connections/find-all-connections-departuring-next) response)))
 
-(defroutes connection-api departuring-next)
+(def departuring-next-by-places
+  (GET (str prefix "/departuring_next/:from/:to") [from to]
+    (let [from-place (place/find-by-name from)
+          to-place (place/find-by-name to)]
+      (-> (connections/find-n-connections-departuring-next from-place to-place 3) response))))
+
+(defroutes connection-api departuring-next departuring-next-by-places)
--- test/busa_server/api/connection_api_test.clj
+++ test/busa_server/api/connection_api_test.clj
@@ -3,10 +3,19 @@
     [midje.sweet :refer :all]
     [ring.mock.request :as mock]
     [busa-server.api.handler :as handler]
-    [busa-server.core.connections :as connections]))
+    [busa-server.core.connections :as connections]
+    [busa-server.model.place :as place]))
 
 (fact "should return HTTP status 200 when finding all connections departuring next"
   (handler/busa-handler
     (mock/request :get "/api/connections/departuring_next")) => (contains {:status 200})
     (provided
       (connections/find-all-connections-departuring-next) => []))
+
+(fact "should return HTTP status 200 when finding connections departuring next"
+  (handler/busa-handler
+    (mock/request :get "/api/connections/departuring_next/nummela/helsinki")) => (contains {:status 200})
+    (provided
+      (place/find-by-name "nummela") => place/nummela
+      (place/find-by-name "helsinki") => place/helsinki
+      (connections/find-n-connections-departuring-next place/nummela place/helsinki 3) => []))
